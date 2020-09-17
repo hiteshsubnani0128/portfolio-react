90b9d6f544afedb97dc9c7452b8c72a6ca939636
Hitesh Subnani <hiteshsubnani734@gmail.com>
2020-09-17 13:10:58 +0530

    Updates

---
+++ precache-manifest.31ecd12a2487d6c67165adaf2db3f6f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8e117744d24488ff289350c33aa6790",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "4e0e99ce896748007bb4",
+    "url": "/portfolio-react/static/css/main.532133f5.chunk.css"
+  },
+  {
+    "revision": "962fa05767b3f409956a",
+    "url": "/portfolio-react/static/js/2.6f1a1bdc.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/portfolio-react/static/js/2.6f1a1bdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e0e99ce896748007bb4",
+    "url": "/portfolio-react/static/js/main.36577cb3.chunk.js"
+  },
+  {
+    "revision": "e71fb2361394a46dcd3a",
+    "url": "/portfolio-react/static/js/runtime-main.1833625d.js"
+  }
+]);
